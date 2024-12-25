3e4424da6c68e04b4f6ee843f061a62cd3951b49
Jan Lucas Reitz <reitz@mmi.rwth-aachen.de>
2024-12-25 15:18:17 +0100

    reorganize unit files and scripts

---
+++ scripts/systemd_install.sh
@@ -0,0 +1,4 @@
+sudo ln -s ~/mmm/systemd/marta.service /etc/systemd/system/marta.service
+sudo ln -s ~/mmm/systemd/power-button-daemon.service /etc/systemd/system/power-button-daemon.service
+sudo ln -s ~/mmm/scripts/cut_power.sh /lib/systemd/system-shutdown/cut_power.sh
+sudo ln -s ~/mmm/scripts/power_button_daemon.py /usr/local/bin/power_button_daemon.py
