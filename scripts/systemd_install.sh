a690a753831db1bad9da1f08ad0d3d78853f62a0
Jan Reitz <jan-reitz@gmx.de>
2024-12-25 15:24:05 +0100

    update systemd installer

--- scripts/systemd_install.sh
+++ scripts/systemd_install.sh
@@ -2,3 +2,7 @@ sudo ln -s ~/mmm/systemd/marta.service /etc/systemd/system/marta.service
 sudo ln -s ~/mmm/systemd/power-button-daemon.service /etc/systemd/system/power-button-daemon.service
 sudo ln -s ~/mmm/scripts/cut_power.sh /lib/systemd/system-shutdown/cut_power.sh
 sudo ln -s ~/mmm/scripts/power_button_daemon.py /usr/local/bin/power_button_daemon.py
+
+sudo systemctl daemon-reload
+sudo systemctl enable marta
+sudo systemctl enable power-button-daemon
